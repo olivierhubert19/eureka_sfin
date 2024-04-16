9e6fde7c263e98d4d8e9372b35f3f6536c6139af
olivierhubert19 <dothanhcong1932002@gmail.com>
2024-04-16 15:16:51 +0700

    Update CRUD Role

---
+++ authen_service/src/main/java/com/example/authen_service/controller/RoleController.java
@@ -0,0 +1,50 @@
+package com.example.authen_service.controller;
+
+import com.example.authen_service.lib.UUID;
+import com.example.authen_service.model.Permission;
+import com.example.authen_service.model.Role;
+import com.example.authen_service.service.PermissionService;
+import com.example.authen_service.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/permission")
+@RequiredArgsConstructor
+public class RoleController {
+    private final RoleService roleService;
+    @GetMapping("/get_all")
+    public ResponseEntity<?> getAllPermission(){
+        List<Role> permissionList = roleService.getAll();
+        if(permissionList.isEmpty()) return ResponseEntity.ok("Does not exist permission");
+        return ResponseEntity.ok(permissionList);
+    }
+
+    @PutMapping("/public/{id}")
+    public ResponseEntity<?> publicPermission(@PathVariable("code") String code){
+        Role role = roleService.findByCode(code);
+        if(role==null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Can't find");
+        else{
+            role.setActive(true);
+            roleService.save(role);
+            return ResponseEntity.ok("update success");
+        }
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody Role role) {
+        try{
+            role.setId(UUID.genID());
+            roleService.save(role);
+            return ResponseEntity.ok("Save success");
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Can't save the permission");
+        }
+
+    }
+
+}
--- authen_service/src/main/java/com/example/authen_service/service/RoleService.java
+++ authen_service/src/main/java/com/example/authen_service/service/RoleService.java
@@ -1,9 +1,12 @@
 package com.example.authen_service.service;
 
+import com.example.authen_service.model.Permission;
 import com.example.authen_service.model.Role;
 import com.example.authen_service.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoleService {
@@ -11,5 +14,20 @@ public class RoleService {
     private RoleRepository roleRepository;
     public Role findByCode(String code){
       return roleRepository.findFirstByCode(code);
+    }
+
+
+
+    public boolean save(Role role){
+        try{
+            roleRepository.save(role);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public List<Role> getAll() {
+        return roleRepository.findAll();
     }
 }
